e2d5af61a5c9fcbee618c0e602bcc1cadfa39088
YouMei2 <azoze9qwertygok2289@gmail.com>
2026-04-01 19:17:00 +0200

    i have added a database of data of user to login to the web-site

---
+++ Back-end/src/main/java/com/hehorhii/restful_api/User.java
@@ -0,0 +1,46 @@
+package com.hehorhii.restful_api;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String user;
+    private String password;
+    private String email;
+
+
+    public User(){}
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getUser() {
+        return user;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
