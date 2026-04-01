496a4946f0769f4f394fc9acc08da61353aca0f0
YouMei2 <azoze9qwertygok2289@gmail.com>
2026-04-01 19:25:53 +0200

    i have added a UserController, a comunication between frontend and backend.

---
+++ Back-end/src/main/java/com/hehorhii/restful_api/UserController.java
@@ -0,0 +1,22 @@
+package com.hehorhii.restful_api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+    @PostMapping User RegisterUser(User user){
+        return userRepository.save(user);
+    }
+}
